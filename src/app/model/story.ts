4dbf3fe584d6a7b22b078e8c50f032f7995740e6
egusahiroaki <egusa.hiroaki@gmail.com>
2017-11-14 23:01:39 +0900

    add story type

---
+++ src/app/model/story.ts
@@ -0,0 +1,11 @@
+export class Story {
+  by: string;
+  descendants: number;
+  id: number;
+  kids: number[];
+  parts: number[];
+  score: number;
+  time: number;
+  title: string;
+  type: string;
+}
